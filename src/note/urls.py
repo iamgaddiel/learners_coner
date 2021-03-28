b98f4f67e00af592a9d0f657ce31e5ab908bc34e
Gaddiel <gaddiel@localhost.localdomain>
2021-03-28 20:55:54 +0100

    fixed note.urls importation problem

--- src/note/urls.py
+++ src/note/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import GetStudentNote, GetStudentNotes, NoteViewSet
+from .views import NoteViewSet
 
 
 router = DefaultRouter()
 router.register('', NoteViewSet)
 
 urlpatterns = [
-    path('user/<phone>/', GetStudentNotes.as_view(),name="user_note_list"),
-    path('user/<int:pk>/<owner>/', GetStudentNote.as_view(), name="user_note")
+    # path('user/<phone>/', GetStudentNotes.as_view(),name="user_note_list"),
+    # path('user/<int:pk>/<owner>/', GetStudentNote.as_view(), name="user_note")
 ]
 
 urlpatterns += router.urls
