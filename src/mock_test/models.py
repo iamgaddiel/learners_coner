44b27101921d9ef51db510ef1eae221647e18de2
Gaddiel <gaddiel@localhost.localdomain>
2021-04-01 21:10:58 +0100

    fixed db table not found

--- src/mock_test/models.py
+++ src/mock_test/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from classroom.models import Subject
+
+
+class MockTest(models.Model):
+    TERMS = [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+    ]
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    term = models.PositiveIntegerField(default=1, choices=TERMS)
+    level = ""
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+class MockTestQuestion(models.Model):
+    mock_test = models.ForeignKey(MockTest, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return super().__str__()
