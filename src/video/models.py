1a3a4ffa75fb61d640d834e2c1f11adbd58bb0e0
Gaddiel <gaddiel@localhost.localdomain>
2021-04-03 21:44:00 +0100

    Video modification

--- src/video/models.py
+++ src/video/models.py
@@ -9,8 +9,8 @@ class Video(models.Model):
     ]
     title = models.CharField(max_length=50, unique=True)
     link = models.CharField(max_length=400, unique=True)
-    level = models.ForeignKey(Class, on_delete=models.CASCADE)
-    term = models.CharField(max_length=3, choices=TERM)
+    level = models.ForeignKey(Class, on_delete=models.CASCADE, null=True)
+    term = models.CharField(max_length=3, choices=TERM, default=TERM[0])
 
     def __str__(self) -> str:
         return f"{self.title} | level: {self.level} | term: {self.term}"
