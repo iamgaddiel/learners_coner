39078d18c8961016f4c7085a2e290648509f146c
Gaddiel <gaddiel@localhost.localdomain>
2021-04-17 01:45:34 +0100

    update

--- src/video/models.py
+++ src/video/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE, SET_NULL
 from classroom.models import Class
 
 class Video(models.Model):
@@ -9,8 +10,8 @@ class Video(models.Model):
     ]
     title = models.CharField(max_length=50, unique=True)
     link = models.CharField(max_length=400, unique=True)
-    level = models.ManyToManyField(Class)
+    level = models.ForeignKey(Class, on_delete=models.CASCADE, null=True)
     term = models.CharField(max_length=3, choices=TERM, default=TERM[0])
 
     def __str__(self) -> str:
-        return f"{self.title} | level: {self.level.title} | term: {self.term}"
+        return f"{self.title} | level: {self.level} | term: {self.term}"
