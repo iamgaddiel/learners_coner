ec4fc28d62a2a89dec85f26bad49f19c3513fa55
Gaddiel <gaddiel@localhost.localdomain>
2021-04-17 14:58:23 +0100

    update mocktest questions

--- src/mock_test/models.py
+++ src/mock_test/models.py
@@ -24,14 +24,21 @@ class MockTestQuestion(models.Model):
         ('c', 'c'),
         ('d', 'd'),
     ]
+    TIME_CHOICES = [
+        ('30', '30'),
+        ('45', '45'),
+        ('60', '60'),
+    ]
     mock_test = models.ForeignKey(MockTest, on_delete=models.CASCADE)
     correct_answer = models.CharField(max_length=1, choices=CORRECT_OPTIONS)
     question = models.CharField(max_length=500, unique=True, blank=True)
     question_number = models.PositiveIntegerField(default=1)
+    timer = models.CharField(max_length=2, choices=TIME_CHOICES, default=TIME_CHOICES[0])
     a = models.CharField(max_length=400, default="")
     b = models.CharField(max_length=400, default="")
-    c = models.CharField(max_length=400, default="")
-    d = models.CharField(max_length=400, default="")
+    c = models.CharField(max_length=400, default="", blank=True)
+    d = models.CharField(max_length=400, default="", blank=True)
+    e = models.CharField(max_length=400, default="", blank=True)
 
     def __str__(self) -> str:
         return f"{self.mock_test.title} | No: {self.question_number} answer: {self.correct_answer}"
